29e1a807b693f3b72296644a9fb7dce8db3a478c
AnjaliNirwal <47429397+AnjaliNirwal@users.noreply.github.com>
2019-02-08 00:36:38 +0530

    Add files via upload

---
+++ mediaplayernew/src/mediaplayernew/Mediaplayernew.java
@@ -0,0 +1,41 @@
+
+package mediaplayernew;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+
+public class Mediaplayernew extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("MEDIA PLAYER");
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent doubleClicked) {
+               if(doubleClicked.getClickCount()== 2){
+              stage.setFullScreen(true);
+            }
+            }
+        });
+       
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
